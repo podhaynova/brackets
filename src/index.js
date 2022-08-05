283c01cda29697ccdca11c6dbd7b85d0f8dcbc95
podhaynova <99250018+podhaynova@users.noreply.github.com>
2022-08-05 10:21:58 +0300

    feat: import brackets solving function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  bracketsConfig = bracketsConfig.map(function(element) {
+         return element.join('');
+      }
+    )
+    while (bracketsConfig.length) {
+      let index = 0;
+  
+      bracketsConfig.forEach(function(element) {
+        if (str.indexOf(element) != -1) {
+          str = str.replace(element, '');
+        } else {
+          index += 1;
+        }
+      });
+      
+      if (index === bracketsConfig.length) {
+     bracketsConfig.length = 0;
+        
+    }
+  }
+    return str.length === 0; 
 }
